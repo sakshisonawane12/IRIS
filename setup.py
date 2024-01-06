9fd2300ae5a04dd912f7da9ba0ab8a8956bb1bf8
sakshisonawane12 <146959039+sakshisonawane12@users.noreply.github.com>
2024-01-06 17:50:03 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='=iris-flower-classifier',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'scikit-learn'
+        
+    ],
+    author='Sonawane Sakshi',
+    author_email='sakshisonawanepatil@gmail.com',
+    description='Iris Flower Classification',
+    url='https://github.com/sakshisonawane12/IRIS',
+    
+)
